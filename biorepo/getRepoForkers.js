9c2978d34b857ef21a69e4df65c6d1ef52e77c4c
auk262 <auk262@psu.edu>
2017-02-15 16:42:25 -0500

    still fixing stuff

--- biorepo/getRepoForkers.js
+++ biorepo/getRepoForkers.js
@@ -4,23 +4,14 @@ forker	repo
 */
 
 // define a file streamer
-var fs = require('fs');
-var reposList = JSON.parse(fs.readFileSync('test.json','utf8'));
+var repo = process.argv[2];
 var forkers = require('../lib/switcher');
 
-var outRepo = [];
-var outUser = [];
-for(r=0; r<reposList.repos.length; r++){ // over all repos
-	// get url
-	var url = reposList.repos[r].concal("/network/memebers");
-	forkers(url,function(err,data){
-		for(i=0; i<data.forkers.length; i++){
-			outRepo.push(reposList.repos[r]);
-			outUser.push(data.forkers[i]);
-		}
-	})
-}
 
-for(i=0; i <outRepo.length; i++){
-	console.log(outUser[i]+"\t"+outUser[i]);
-}
+
+var url = repo.concat("/network/members");
+forkers(url,function(err,data){
+	for(f=0; f<data.forkers.length; f++){
+		console.log(data.forkers[f]+"\t"+url.replace("/network/members",""));
+	}
+})
